60cad13b94d3482016d45ee22c50cd1332e03c2c
Roman Yakimkin <r.yakimkin@yandex.ru>
2018-12-23 17:18:47 +0300

    Admin dashboard added

---
+++ laravel/resources/views/admin/dashboard.blade.php
@@ -0,0 +1,50 @@
+@extends('admin.layouts.app_admin')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Категорий</span></p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Материалов</span></p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Посетителей</span></p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Сегодня</span></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-6">
+                <a class="btn btn-block btn-default" href="#">Создать категорию</a>
+                <a class="list-group-item" href="#">
+                    <h4 class="list-group-item-heading">Категория первая</h4>
+                    <p class="list-group-item-text">
+                        Кол-во метариалов
+                    </p>
+                </a>
+            </div>
+            <div class="col-sm-6">
+                <a class="btn btn-block btn-default" href="#">Создать материал</a>
+                <a class="list-group-item" href="#">
+                    <h4 class="list-group-item-heading">Материал первый</h4>
+                    <p class="list-group-item-text">
+                        Категория
+                    </p>
+                </a>
+            </div>
+        </div>
+    </div>
+
+@endsection
