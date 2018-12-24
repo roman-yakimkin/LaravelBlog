159023f0af5bc087549ade283a8ac6bf22df20f8
Roman Yakimkin <r.yakimkin@yandex.ru>
2018-12-24 15:30:30 +0300

    Edit form for categiries added

--- laravel/app/Category.php
+++ laravel/app/Category.php
@@ -2,9 +2,20 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
-    //
+    protected $fillable = ['title', 'slug', 'parent_id', 'published', 'created_by', 'modified_by'];
+
+    public function setSlugAttribute($value){
+        $this->attributes['slug'] = Str::slug(mb_substr($this->title, 0, 40) . '-'. Carbon::now()->format("dmyHi"), '-');
+    }
+
+    // Get children categories
+    public function children(){
+        return $this->hasMany(self::class, 'parent_id');
+    }
 }
--- laravel/resources/views/admin/categories/index.blade.php
+++ laravel/resources/views/admin/categories/index.blade.php
@@ -8,32 +8,33 @@
             @slot("parent") Главная @endslot
             @slot("active") Категории @endslot
         @endcomponent
-    </div>
 
-    <a href="{{route("admin.category.create")}}" class="btn btn-primary pull-right"><i class="fa fa-plus-square-o"></i>Создать категорию</a>
+        <hr />
 
-    <table class="table table-striped">
-        <thead>
+        <a href="{{route("admin.category.create")}}" class="btn btn-primary pull-right"><i class="fa fa-plus-square-o"></i>Создать категорию</a>
+
+        <table class="table table-striped">
+            <thead>
             <th>Наименование</th>
             <th>Публикация</th>
-            <th>Действующие</th>
-        </thead>
-        <tbody>
-        @forelse($categories as $category)
-            <tr>
-                <td>{{$category->title}}</td>
-                <td>{{$category->published}}</td>
-                <td>
-                    <a href="{{route("admin.category.edit", ["id" => $category->id])}}"><i class="fa fa-edit"></i></a>
-                </td>
-            </tr>
-        @empty
-            <tr>
-                <td colspan="3" class="text-center"><h2>Данные отсутствуют</h2></td>
-            </tr>
-        @endforelse
-        </tbody>
-        <tfoot>
+            <th>Действия</th>
+            </thead>
+            <tbody>
+            @forelse($categories as $category)
+                <tr>
+                    <td>{{$category->title}}</td>
+                    <td>{{$category->published}}</td>
+                    <td>
+                        <a href="{{route("admin.category.edit", ["id" => $category->id])}}"><i class="fa fa-edit"></i></a>
+                    </td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="3" class="text-center"><h2>Данные отсутствуют</h2></td>
+                </tr>
+            @endforelse
+            </tbody>
+            <tfoot>
             <tr>
                 <td colspan="3">
                     <ul class="pagination pull-right">
@@ -41,7 +42,9 @@
                     </ul>
                 </td>
             </tr>
-        </tfoot>
-    </table>
+            </tfoot>
+        </table>
+
+    </div>
 
 @endsection
